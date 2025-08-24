b518e5521bb83a2e4316dd3ae96ca3deb0903789
Huzaifa Ali <huzaifa2122alii@gmail.com>
2025-08-24 10:07:13 +0500

    Update Emailjs.js

--- Emailjs.js
+++ Emailjs.js
@@ -7,12 +7,12 @@
 
 
   document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); 
 
-    emailjs.sendForm('service_d4zebqg', 'template_nk01fxj', this)
+    emailjs.sendForm('service_gjqt25k', 'template_s6jhw1t', this)
       .then(function() {
         alert('✅ Message sent successfully!');
       }, function(error) {
         alert('❌ Failed to send message: ' + JSON.stringify(error));
       });
-  });
+  }); 
